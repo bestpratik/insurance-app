297dbd36c60d563e71a18b8acb9383441b8f617d
devsujoy18 <43414517+devsujoy18@users.noreply.github.com>
2025-05-26 20:59:19 +0530

    Sidebar link changed

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,9 @@ Route::middleware('auth')->group(function () {
     /*All Purchase List*/
     Route::get('/all-purchase-list', function () {
         return view('purchase.all_list');
-    });
+    })->name('purchase.list');
 
-    Route::get('purchase-list', [PurchaseController::class, 'purchaseList'])->name('purchase.list');
+    //Route::get('purchase-list', [PurchaseController::class, 'purchaseList'])->name('purchase.list');
     Route::get('purchase/edit/{id}', [PurchaseController::class, 'purchaselist_edit'])->name('purchase.edit');
     // Route::get('purchase-success', [PurchaseController::class, 'successPage'])->name('purchase.success');
     Route::get('purchase-success/{id}', [PurchaseController::class, 'successPage'])->name('purchase.success');
