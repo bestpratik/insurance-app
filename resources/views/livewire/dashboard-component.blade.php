e5128e8604e06d645831a5392c72d26504ea9017
saratdbt <sarat.dbt@gmail.com>
2025-06-10 19:45:35 +0530

    text style change

--- resources/views/livewire/dashboard-component.blade.php
+++ resources/views/livewire/dashboard-component.blade.php
@@ -4,7 +4,7 @@
          <h4 class="text-xl font-bold text-start">Policy sold in 7 days</h4>
         <div class="sm:w-64 my-3">
             <label for="storeFilter" class="text-sm font-medium text-gray-700 mb-1">Per Page</label>
-            <select wire:model.live="perPage" class="pbg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 inline min-w-[60px] py-1">
+            <select wire:model.live="perPage" class="pbg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 inline min-w-[60px] py-1">
                 <option value="10">10</option>
                 <option value="25">25</option>
                 <option value="50">50</option>
