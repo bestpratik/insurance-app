2d8f3c330f83696c7c41ef07f7d99b346d6e104a
*Anuradha-DBT* <*anuradham.dbt@gmail.com*>
2025-07-30 17:22:25 +0530

    fix navbar issue

--- resources/views/layouts/front_navigation.blade.php
+++ resources/views/layouts/front_navigation.blade.php
@@ -1,31 +1,38 @@
-  <section class="bg-red-700 text-white text-sm px-6 py-3 flex justify-between items-center">
+@php
+    $contact = contactHelper();
+    //dd($contact);
+@endphp
+ 
+ <section class="bg-red-700 text-white text-sm px-6 py-3 flex justify-between items-center">
         <div class="flex items-center space-x-6">
             <div class="flex items-center space-x-3">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="size-5">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-                </svg>
-                @php
-                    $contact = contactHelper();
-                    //dd($contact);
-                @endphp
+                <a href="tel:{{$contact->phone}}" class="text-white hover:underline">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                    </svg>
+                </a>
+                
                 <span class="hidden md:block">
-                    <a href="tel:{{$contact->phone}}" class="text-blue-500 hover:underline">
+                    <a href="tel:{{$contact->phone}}" class="text-white hover:underline">
                         {{$contact->phone}}
                     </a>
                 </span>
             </div>
             <div class="flex items-center space-x-3">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
-                    stroke="currentColor" class="size-5">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                </svg>
+                <a href="mailto:{{ $contact->email }}" class="text-white hover:underline">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-5 h-5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                    </svg>
+                </a>
+
 
                 <span class="hidden md:block">
-                    <a href="mailto:{{$contact->email}}" class="text-blue-500 hover:underline">
-                        {{$contact->email}}
+                    <a href="mailto:{{ $contact->email }}" class="text-white hover:underline">
+                        {{ $contact->email }}
                     </a>
                 </span> 
             </div>
