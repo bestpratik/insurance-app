1680a98c0cf00375ff349e4f72f14d443e6c5242
*Anuradha-DBT* <*anuradham.dbt@gmail.com*>
2025-08-13 20:12:15 +0530

    fix mail template issue

--- resources/views/purchase/pdfs/insurance_dynamic_document_email.blade.php
+++ resources/views/purchase/pdfs/insurance_dynamic_document_email.blade.php
@@ -6,12 +6,7 @@
 <body>
    
 
-    @php
-
-        $grossPremium = (float) $data['templatebodyValue'][9];
-        $insurancePremiumTax = (float) $data['templatebodyValue'][8];
-        $payableAmount = $grossPremium + $insurancePremiumTax;
-
+    <!-- @php
         $placeholders = [
             '%InsuranceName%' => $data['templatebodyValue'][0],
             '%policyNo%' => $data['templatebodyValue'][1],
@@ -30,8 +25,35 @@
         ];
 
         $output_string = str_replace(array_keys($placeholders), array_values($placeholders), $data['templateBody']);
-    @endphp
+    @endphp -->
+
+
+    @php
+    $grossPremium = (float) $data['templatebodyValue'][9];
+    $insurancePremiumTax = (float) $data['templatebodyValue'][8];
+    $payableAmount = $grossPremium + $insurancePremiumTax;
+
+    $placeholders = [
+        '%InsuranceName%' => $data['templatebodyValue'][0],
+        '%policyNo%' => $data['templatebodyValue'][1],
+        '%policyHolderAddress1%' => $data['templatebodyValue'][2],
+        '%policyStartdate%' => $data['templatebodyValue'][3],
+        '%policyEnddate%' => $data['templatebodyValue'][4],
+        '%purchaseDate%' => $data['templatebodyValue'][5],
+        '%policyTerm%' => $data['templatebodyValue'][6],
+        '%netAnnualpremium%' => $data['templatebodyValue'][7],
+        '%insurancePremiumtax%' => number_format($insurancePremiumTax, 2),
+        '%grossPremium%' => number_format($grossPremium, 2),
+        '%payableAmount%' => number_format($payableAmount, 2), // new placeholder
+        '%rentAmount%' => $data['templatebodyValue'][10],
+        '%riskAddress%' => $data['templatebodyValue'][11],
+        '%insurerTitle%' => $data['templatebodyValue'][12],
+        '%detailsofCover%' => $data['templatebodyValue'][13],
+    ];
+
+    $output_string = str_replace(array_keys($placeholders), array_values($placeholders), $data['templateBody']);
+@endphp
 
     {!! $output_string !!}
-</body> 
+</body>
 </html>
