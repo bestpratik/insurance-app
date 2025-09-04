2b1f3a0066dd04794a0d5adcc180dcc171f6518f
*Anuradha-DBT* <*anuradham.dbt@gmail.com*>
2025-09-04 20:12:44 +0530

    fix title issue

--- resources/views/components/front.blade.php
+++ resources/views/components/front.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{'Money Wise Plc'}}</title>
+    <title>{{ config('app.name', 'UCMASWB') }}</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
