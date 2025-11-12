bf712740304e50e1983dc5fc34816a0633456032
Akasdeep1999 <akashb.dbt@gmail.com>
2025-11-12 20:01:14 +0530

    Update front.blade.php

--- resources/views/components/front.blade.php
+++ resources/views/components/front.blade.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<!-- CSRF Token -->
-<meta name="csrf-token" content="{{ csrf_token() }}">
 
 <head>
     <meta charset="UTF-8">
